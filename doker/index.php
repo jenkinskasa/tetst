92910c35a1ba4f0dde32fb764009b76d63f938cd
ebarbat <ebarbat@ebarbats-MacBook-Pro.local>
2021-06-03 19:15:59 +0300

    dobavi eks , dockerfile

---
+++ doker/index.php
@@ -0,0 +1,8 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello frof kubernetes</h1><br>";
+echo "Server IP adress is : $ip_server","<br><p>";
+echo "spizgeno <front color=blue>";
+
+?>
