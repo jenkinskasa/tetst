e845e411fe9c7740b5e91e0912a6139e8118436a
ebarbat <ebarbat@ebarbats-MacBook-Pro.local>
2022-03-15 18:48:30 +0200

    back change index.php

--- all/index.php
+++ all/index.php
@@ -13,7 +13,7 @@ $pass = 'rootpassword';
 // check the MySQL connection status
 $conn = new mysqli($host, $user, $pass);
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error-sSUKA);
+    die("Connection failed: " . $conn->connect_error);
 } else {
     echo "Connected to MySQL server successfully!";
 }
